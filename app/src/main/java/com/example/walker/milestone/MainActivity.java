3622350eaab66f7ac70b5b9d888007d2acef6bdc
Bryan <b.lim@berkeley.edu>
2018-11-20 22:06:46 -0800

    Creating new user working and firebase connects

--- app/src/main/java/com/example/walker/milestone/MainActivity.java
+++ app/src/main/java/com/example/walker/milestone/MainActivity.java
@@ -4,19 +4,148 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     public Intent intent;
+    private EditText username;
+    private EditText password;
+    private Button signup;
+    private Button login;
+    private Button forgotPassword;
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Set our variables from frontend
+        // EditTexts
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        // Buttons
+        signup = findViewById(R.id.signup);
+        login = findViewById(R.id.login);
+        forgotPassword = findViewById(R.id.forgot_password);
+
+        // Set the listeners
+        signup.setOnClickListener(this);
+        login.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+    }
+
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = username.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            username.setError("Required.");
+            valid = false;
+        } else {
+            username.setError(null);
+        }
+
+        String pass = password.getText().toString();
+        if (TextUtils.isEmpty(pass)) {
+            password.setError("Required.");
+            valid = false;
+        } else {
+            password.setError(null);
+        }
+
+        return valid;
+    }
+
+    @Override
     public void onClick(View view) {
         intent = new Intent(this, AccountType.class);
         startActivity(intent);
+
+        int i = view.getId();
+        if (i == R.id.signup) {
+            createAccount(username.getText().toString(), password.getText().toString());
+        } else if (i == R.id.login) {
+            signIn(username.getText().toString(), password.getText().toString());
+        }
     }
 }
