fe029e146e62e5aad4301ac87d5cd5c3d6e896a0
Sonia Uppal <Sonia@Sonias-MacBook-Pro.local>
2018-12-02 10:01:17 -0800

    bottom nav bar and settings

--- app/src/main/java/com/example/walker/milestone/SupporterSettings.java
+++ app/src/main/java/com/example/walker/milestone/SupporterSettings.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SupporterSettings extends AppCompatActivity {
 
@@ -13,6 +14,17 @@ public class SupporterSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supporter_settings);
+
+        Button[] buttons = new Button[2];
+        Button b1 = findViewById(R.id.milestone_reached);
+        buttons[0] = b1;
+        Button b2 = findViewById(R.id.sobriety_reset);
+        buttons[1] = b1;
+
+        for (int i = 0; i < buttons.length; i++){
+            buttons[i].setTag("on");
+            buttons[i].setBackgroundResource(R.drawable.notification_on);
+        }
     }
 
     public void onClick(View view) {
@@ -28,5 +40,19 @@ public class SupporterSettings extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+    public void onOffNotification(View view) {
+        int id = view.getId();
+        Button b = findViewById(id);
+        if(b.getTag() == "off") {
+            b.setBackgroundResource(R.drawable.notification_on);
+            b.setTag("on");
+        } else {
+            b.setBackgroundResource(R.drawable.notification_off);
+            b.setTag("off");
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/example/walker/milestone/UserSettings.java
+++ app/src/main/java/com/example/walker/milestone/UserSettings.java
@@ -1,9 +1,11 @@
 package com.example.walker.milestone;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class UserSettings extends AppCompatActivity {
 
@@ -13,6 +15,19 @@ public class UserSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+
+        Button[] buttons = new Button[3];
+        Button b1 = findViewById(R.id.milestone_reached);
+        buttons[0] = b1;
+        Button b2 = findViewById(R.id.supporter_request);
+        buttons[1] = b1;
+        Button b3 = findViewById(R.id.daily_update);
+        buttons[2] = b3;
+
+        for (int i = 0; i < buttons.length; i++){
+            buttons[i].setTag("on");
+            buttons[i].setBackgroundResource(R.drawable.notification_on);
+        }
     }
 
     public void onClick(View view) {
@@ -32,6 +47,19 @@ public class UserSettings extends AppCompatActivity {
         } else if (id == R.id.resourcesButton){
             intent = new Intent(this, UserResources.class);
             startActivity(intent);
+        }
+
+    }
+
+    public void onOffNotification(View view) {
+        int id = view.getId();
+        Button b = findViewById(id);
+        if(b.getTag() == "off") {
+            b.setBackgroundResource(R.drawable.notification_on);
+            b.setTag("on");
+        } else {
+            b.setBackgroundResource(R.drawable.notification_off);
+            b.setTag("off");
         }
 
     }
